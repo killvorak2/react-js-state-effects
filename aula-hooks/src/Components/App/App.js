c998c178cb03fa1e2f973ba19c60e26a16eb7778
killvorak2 <marcelo.ceribelli@gmail.com>
2021-07-16 15:10:11 -0300

    base inicial + componentes e css

---
+++ aula-hooks/src/Components/App/App.js
@@ -0,0 +1,13 @@
+import SmartCounter from "../Smartcounter/Smartcounter";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <SmartCounter />
+    </>
+  );
+}
+
+export default App;
